5481dd5c4f72b1114b6c9b52996d38cc523381fc
poro912 <40479447+poro912@users.noreply.github.com>
2022-11-15 19:54:26 +0900

    DLL update

--- MyMate_Network_Library/MyMate_Network_Library/Default.cs
+++ MyMate_Network_Library/MyMate_Network_Library/Default.cs
@@ -10,10 +10,12 @@ namespace Default
 	{
 		// 기본값 설정
 		public const int port = 8090;
-#if DEBUG
-		public const string Address = "127.0.0.1";
-#elif RELEASE
 		public const string Address = "172.18.7.140";
+
+#if DEBUG
+		//public const string Address = "127.0.0.1";
+#elif RELEASE
+		
 #endif
 		public const string IP = "172.18.7.140";
 		public const string SubNetmask = "255.255.255.0";
